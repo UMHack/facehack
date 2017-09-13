6ce3bbf0c3cc1f9f08b29b71287a2fefbe1a26dd
June <jeff.bluesky13@gmail.com>
2017-09-13 10:20:48 +0800

    Remove unnecessary import and escape character

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { Container } from 'semantic-ui-react'
 import { Link, Element, Events, scrollSpy } from 'react-scroll'
 import Navbar from './components/Navbar'
 import Header from './components/Header'
